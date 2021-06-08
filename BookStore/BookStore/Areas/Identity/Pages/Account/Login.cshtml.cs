ef4ee04e3620b5ce3a0dd5355e65c1f774d0c609
Yanka Domenikova <yankadomenikova@gmail.com>
2021-06-08 21:15:56 +0300

    Add username to register form

--- BookStore/BookStore/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BookStore/BookStore/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -80,9 +80,10 @@ namespace BookStore.Areas.Identity.Pages.Account
         
             if (ModelState.IsValid)
             {
+                User user = await this._userManager.FindByEmailAsync(Input.Email);
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");

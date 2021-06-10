6d708a81105edc5ad1a25fed9235b30397b44db1
Yanka Domenikova <yankadomenikova@gmail.com>
2021-06-10 21:09:42 +0300

    Add services

--- BookStore/BookStore/Data/ApplicationDbContext.cs
+++ BookStore/BookStore/Data/ApplicationDbContext.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
+using BookStore.Models;
+
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +11,9 @@ namespace BookStore.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ BookStore/BookStore/Services/BookService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BookStore.Services.Interfaces;
+
+namespace BookStore.Services
+{
+    public class BookService : IBookService
+    {
+    }
+}

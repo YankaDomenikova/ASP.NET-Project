f0c3fb9cd2fb10c41759bd98de1fbb25961b4cc1
Yanka Domenikova <yankadomenikova@gmail.com>
2021-06-12 20:41:28 +0300

    Add style to book view

---
+++ Application/Application/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult AddToCart()
+        {
+            return View("Index");
+        }
+    }
+}
